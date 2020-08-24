b2abbae3918aa13d5e30fd568b7c0afea524c887
Jan Kristinus <jan.kristinus@yakamara.de>
2020-08-24 16:55:13 +0200

    Fehler bei 0.9 um Tableset Import behoben

--- install.php
+++ install.php
@@ -6,7 +6,7 @@ rex_config::set('simple_oauth', 'encryption_key', base64_encode(random_bytes(32)
 
 /** @var rex_addon $this */
 
-$content = rex_file::get(rex_path::addon('ycom', 'install/tablesets/yform_oauth_tables.json'));
+$content = rex_file::get(rex_path::addon('simple_oauth', 'install/tablesets/yform_oauth_tables.json'));
 rex_yform_manager_table_api::importTablesets($content);
 
 rex_delete_cache();
